72e691bbb9598af690008282531383e347053258
SEO <SEO@DESKTOP-MS3J9SF>
2023-09-07 17:29:14 +0900

    test

---
+++ vue_prj/src/main/java/com/ex/vue/vue_prj/VuePrjApplication.java
@@ -0,0 +1,13 @@
+package com.ex.vue.vue_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuePrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuePrjApplication.class, args);
+	}
+
+}
---
+++ vue_prj/src/test/java/com/ex/vue/vue_prj/VuePrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.vue.vue_prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuePrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
